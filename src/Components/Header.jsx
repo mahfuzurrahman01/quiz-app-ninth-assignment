4fcc38bb6634328d51511ad475f69f81642a37b3
mahfuzurrahman01 <mahfuzurrahman01010101@gmail.com>
2022-10-10 22:13:20 +0600

    navbar design done

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,13 +1,20 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-
+import { NavLink } from 'react-router-dom';
+import logo from '../../src/images/navbar-logo.png'
 const Header = () => {
     return (
-        <div>
-            <Link to='/home'>Home</Link>
-            <Link to='/quiz'>Quiz</Link>
-            <Link to='/statistics'>Statistics</Link>
-            <Link to='/blog'>Blog</Link>
+        <div className='bg-slate-800 py-2 px-14 flex justify-between items-center'>
+            <div className='flex items-center'>
+                <img src={logo} className='w-20 h-20' alt="" />
+                <p className='text-3xl font-normal text-white font-sans'>Delta Quiz</p>
+            </div>
+            <div className='flex justify-end items-center gap-8
+            '>
+                <NavLink className={({ isActive }) => isActive ? 'bg-slate-100 duration-300 text-black px-3 py-2 rounded text-sm font-semibold font-sans' : 'bg-red-500 duration-300 hover:bg-red-700 px-3 py-2 rounded text-white text-sm font-semibold font-sans'} to='/home'>Home</NavLink>
+                <NavLink className={({ isActive }) => isActive ? 'bg-slate-100 duration-300 text-black px-3 py-2 rounded text-sm font-semibold font-sans' : 'bg-red-500 duration-300 hover:bg-red-700 px-3 py-2 rounded text-white text-sm font-semibold font-sans'} to='/quiz'>Quiz</NavLink>
+                <NavLink className={({ isActive }) => isActive ? 'bg-slate-100 duration-300 text-black px-3 py-2 rounded text-sm font-semibold font-sans' : 'bg-red-500 duration-300 hover:bg-red-700 px-3 py-2 rounded text-white text-sm font-semibold font-sans'} to='/statistics'>Statistics</NavLink>
+                <NavLink className={({ isActive }) => isActive ? 'bg-slate-100 duration-300 text-black px-3 py-2 rounded text-sm font-semibold font-sans' : 'bg-red-500 duration-300 hover:bg-red-700 px-3 py-2 rounded text-white text-sm font-semibold font-sans'} to='/blog'>Blog</NavLink>
+            </div>
         </div>
     );
 };
