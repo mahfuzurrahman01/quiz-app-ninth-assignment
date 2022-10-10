c1a1300e893b0eb6bb8f7f95a78e8904dec9fde8
mahfuzurrahman01 <mahfuzurrahman01010101@gmail.com>
2022-10-11 01:27:14 +0600

    cards are responsive

---
+++ src/Components/QuizCart.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const QuizCart = ({ item }) => {
+    return (
+        <div className='bg-slate-300 bg-opacity-80 relative rounded-md p-4 shadow-neutral-400 shadow-lg'>
+            <div>
+                <img src={item.logo} alt="" />
+            </div>
+            <div>
+                <p className='bg-slate-700 inline text-gray-300 text-xs py-1 px-2 rounded-lg my-1 absolute top-1 right-2'>Total quiz: {item.total}</p>
+            </div>
+            <div className='flex justify-between items-center'>
+                <p className='text-2xl bg-slate-200 rounded my-2 py-1 px-7 font-bold font-sans text-slate-700'>{item.name}</p>
+                <button className='bg-red-500 py-1 px-7 text-lg hover:bg-red-700 duration-300 text-white rounded'>Start</button>
+            </div>
+        </div>
+    );
+};
+
+export default QuizCart;
