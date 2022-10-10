7dcfb0b876e41ecac672244d5f9d8681d8d31db9
mahfuzurrahman01 <mahfuzurrahman01010101@gmail.com>
2022-10-10 21:37:15 +0600

    router defiened and title and logo change

--- src/App.js
+++ src/App.js
@@ -1,10 +1,42 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './App.css';
-
+import Blog from './Components/Blog';
+import Home from './Components/Home';
+import Main from './Components/Main';
+import Quiz from './Components/Quiz';
+import Statistics from './Components/Statistics';
+const router = createBrowserRouter([
+  {
+    path:'/',
+    element: <Main></Main>,
+    children: [
+      {
+        path: '/',
+        element: <Home></Home>
+      },
+      {
+        path: '/home',
+        element: <Home></Home>
+      },
+      {
+        path: '/quiz',
+        element: <Quiz></Quiz>
+      },
+      {
+        path: '/statistics',
+        element: <Statistics></Statistics>
+      },
+      {
+        path: '/blog',
+        element: <Blog></Blog>
+      }
+    ]
+  }
+])
 function App() {
   return (
-    <div className="App">
-     <h1>hello world</h1>
-    </div>
+   <RouterProvider router={router}>
+   </RouterProvider>
   );
 }
 
---
+++ src/Components/Statistics.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Statistics = () => {
+    return (
+        <div>
+            <h1>THIS IS THE CHART SECTION</h1>
+        </div>
+    );
+};
+
+export default Statistics;<h1>THIS IS THE CHART SECTION</h1>
